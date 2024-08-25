d9cc8459ab6623d1911db41d0bc9051f473e7632
balankdharan <balankdharan@gmail.com>
2024-08-25 17:58:48 +0530

    changes

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
+import Displayalbum from "./Displayalbum";
 
 const Display = () => {
   return (
     <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<Displayalbum />} />
       </Routes>
     </div>
   );
